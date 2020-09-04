59c977c4287c53793e00e11065f98e90d27e558d
mukul-kmr-jha <jimickeyji@gmail.com>
2020-09-04 17:33:13 +0100

    feat: WIP page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="main-container">
+        <h1>HELLO 	&#128075;, I'M MUKUL KUMAR JHA!</h1>
+        <span> <i>I'm experienced with React, Redux, React-Native, NextJS, S/CSS, Antd, Typescript, GraphQL, Node, Express.js, Firebase and lots of other technologies</i> &#128526;</span>
+      </div>
+      <div className="links-container">
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            id="linkedin"
+            href={"https://www.linkedin.com/in/mukul-kr-jha/"}
+            aria-label={"LinkedIn Profile"}
+            tabIndex={0}
+        />
+        <a
+            id="github"
+            href={"https://github.com/mukul-kmr-jha"}
+            aria-label={"Github Profile"}
+            tabIndex={0}
+        />
+        <a
+            id="stackoverflow"
+            href={"https://stackoverflow.com/users/9312233/code-it-up?tab=profile"}
+            aria-label={"StackOverflow Profile"}
+            tabIndex={0}
+        />
+          <a
+              id="twitter"
+              href={"https://twitter.com/Mukulkrjha2"}
+              aria-label={"Twitter Profile"}
+              tabIndex={0}
+          />
+      </div>
     </div>
   );
 }
